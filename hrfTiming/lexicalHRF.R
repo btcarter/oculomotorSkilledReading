70fc368c97c1ee2d29c434067981a4994a6eb35b
Benjamin Carter <benjamincarter@Benjamins-MBP-010037184133.app.byu.edu>
2019-01-25 16:41:25 -0700

    new HRF building scripts

---
+++ hrfTiming/lexicalHRF.R
@@ -0,0 +1,56 @@
+#This script builds the lexical HRF, which is to be used as a baseline regressor of no interest since we're only interested in oculomotor control during reading rather than linguistic contributions.
+
+#Set the working environment.
+setwd("~/Box/LukeLab/SkilledReadingStudy/workingDirectory/timingFiles/oculomotor")
+
+#create the directory to hold timing files.
+if (file.exists("ortho")){
+  setwd("ortho")
+} else {
+  dir.create("ortho")
+  setwd("ortho")
+}
+
+#Read in source csv as matrix
+group <- read.csv("~/Box/LukeLab/SkilledReadingStudy/workingDirectory/timingFiles/source_csvs/fixvsac.csv")
+colnames(group)
+
+#remove unneeded columns/values
+group = group[group$Content_Or_Function == "Content", ]
+group <- group[,c("RECORDING_SESSION_LABEL","RUN","START_TIME","OrthoMatchModel","IA_FIRST_RUN_DWELL_TIME")]
+
+#remove NA values form group matrix
+group = group[is.na(group$START_TIME) == FALSE, ]
+group = group[is.na(group$OrthoMatchModel) == FALSE, ]
+
+#Create a column with times in parametric format ([event1 time]*[orthographic]*[POS]*[LSA] ...  [eventn time]*[orthographic]*[POS]*[LSA]) and perform maths to convert times from milliseconds to seconds.
+#group$Parametric_times = paste((group$START_TIME/1000), log10(group$OrthoMatchModel), sep = "*")
+group$Parametric_times = log(group$OrthoMatchModel)
+group$Parametric_times = paste((group$START_TIME/1000), (group$Parametric_times + abs(min(group$Parametric_times))+1), sep ="*")
+group$Parametric_times = paste(group$Parametric_times,(group$IA_FIRST_RUN_DWELL_TIME/1000), sep = ":")
+
+mdata = group
+colnames(mdata)
+mdata <- mdata[c(1,2,6)]
+library(reshape2)
+mdata <- melt(mdata, id=c("RECORDING_SESSION_LABEL","RUN"))
+mdata = mdata[is.na(mdata$RECORDING_SESSION_LABEL) == FALSE, ]
+
+#Assemble the individual AM timing files so that each subject has an individual timing file with one row per run
+for (i in unique(mdata$RECORDING_SESSION_LABEL)) {
+  sub1data = mdata[mdata$RECORDING_SESSION_LABEL == i, ]
+  colnames(sub1data)
+  if (nrow(sub1data) > 0) {
+    sub1data = sub1data[c(2:4)]
+    sub1data$variable = 1:nrow(sub1data)
+    sub1data[sub1data$RUN == 3, ]$variable = sub1data[sub1data$RUN == 3, ]$variable - max(sub1data[sub1data$RUN == 2, ]$variable)
+    sub1data[sub1data$RUN == 2, ]$variable = sub1data[sub1data$RUN == 2, ]$variable - max(sub1data[sub1data$RUN == 1, ]$variable)
+    sub1data = dcast(sub1data, RUN ~ variable)
+    #max(sub1data[sub1data$RUN == 3, ]$variable)
+    sub1data = sub1data[2:ncol(sub1data)]
+    write.table(sub1data, paste(i, ".txt", sep = ""), sep = "\t", na = "", col.names = FALSE, row.names = FALSE, quote = FALSE)
+    #write.table(sub1data, paste("Luke_Reading_S", i, ".txt", sep = ""), sep = "\t", col.names = FALSE, row.names = FALSE) 
+  }
+}
+
+summary(mdata$RECORDING_SESSION_LABEL)
---
+++ hrfTiming/saccades.R
@@ -0,0 +1,59 @@
+# This script builds the HRF for saccades during reading.
+
+#Set the working environment.
+setwd("~/Box/LukeLab/SkilledReadingStudy/workingDirectory/timingFiles/oculomotor")
+
+################
+#SACCADE EVENTS#
+################
+
+#Read in fixvsac.csv as table.
+group <- read.csv("~/Box/LukeLab/SkilledReadingStudy/workingDirectory/timingFiles/source_csvs/fixvsac.csv")
+colnames(group)
+
+#CLEAN UP THE INPUT
+#remove NA values form group matrix
+group = group[is.na(group$START_TIME_FIX) == FALSE, ]
+group = group[is.na(group$FIX_DURATION) == FALSE, ]
+
+#Remove leftward saccades
+group <- group[which(group$CURRENT_SAC_DIRECTION=='RIGHT'),]
+
+#Trim off longer saccades
+group <- group[which(group$CURRENT_SAC_AMPLITUDE<4),]
+
+#plot(density(group$CURRENT_SAC_AMPLITUDE))
+
+#Create column with regressors of interest
+#Convert times to milliseconds
+group$START_TIME_FIX = group$START_TIME_FIX/1000
+group$FIX_DURATION = group$FIX_DURATION/1000
+
+#Add saccade amplitude as amplitude modulator
+group$Parametric = paste((group$START_TIME_FIX), group$CURRENT_SAC_AMPLITUDE, sep = "*")
+group$Parametric = paste(group$Parametric, group$FIX_DURATION, sep = ":")
+
+#Remove unneeded columns
+mdata = group
+colnames(mdata)
+mdata <- mdata[c(1,4,9)]
+library(reshape2)
+mdata <- melt(mdata, id=c("RECORDING_SESSION_LABEL","RUN"))
+mdata = mdata[is.na(mdata$RECORDING_SESSION_LABEL) == FALSE, ]
+
+#Assemble the individual AM timing files so that each subject has an individual timing file with one row per run
+for (i in unique(mdata$RECORDING_SESSION_LABEL)) {
+  sub1data = mdata[mdata$RECORDING_SESSION_LABEL == i, ]
+  colnames(sub1data)
+  if (nrow(sub1data) > 0) {
+    sub1data = sub1data[c(2:4)]
+    sub1data$variable = 1:nrow(sub1data)
+    sub1data[sub1data$RUN == 3, ]$variable = sub1data[sub1data$RUN == 3, ]$variable - max(sub1data[sub1data$RUN == 2, ]$variable)
+    sub1data[sub1data$RUN == 2, ]$variable = sub1data[sub1data$RUN == 2, ]$variable - max(sub1data[sub1data$RUN == 1, ]$variable)
+    sub1data = dcast(sub1data, RUN ~ variable)
+    sub1data = sub1data[2:ncol(sub1data)]
+    write.table(sub1data, paste("saccades/",i,".txt", sep = ""), sep = "\t", na = "", col.names = FALSE, row.names = FALSE, quote = FALSE)
+    }
+}
+
+summary(mdata$RECORDING_SESSION_LABEL)
