a87b0da737151d4162f8f0dc229da57bbef6c7d3
btcarter <benjamin.t.carter@gmail.com>
2019-02-04 10:45:56 -0700

    fixing

--- hrfTiming/lexicalHRF.R
+++ hrfTiming/lexicalHRF.R
@@ -5,10 +5,10 @@ setwd("~/Box/LukeLab/SkilledReadingStudy/workingDirectory/timingFiles/oculomotor
 
 #create the directory to hold hrf files.
 if (file.exists("linguistic")){
-  setwd("linguistic")
+    setwd("linguistic")
 } else {
-  dir.create("linguistic")
-  setwd("linguistic")
+    dir.create("linguistic")
+    setwd("linguistic")
 }
 
 #Read in source csv as matrix
@@ -36,17 +36,17 @@ mdata = mdata[is.na(mdata$RECORDING_SESSION_LABEL) == FALSE, ]
 
 #Assemble the individual AM timing files so that each subject has an individual timing file with one row per run
 for (i in unique(mdata$RECORDING_SESSION_LABEL)) {
-  sub1data = mdata[mdata$RECORDING_SESSION_LABEL == i, ]
-  colnames(sub1data)
-  if (nrow(sub1data) > 0) {
-    sub1data = sub1data[c(2:4)]
-    sub1data$variable = 1:nrow(sub1data)
-    sub1data[sub1data$RUN == 3, ]$variable = sub1data[sub1data$RUN == 3, ]$variable - max(sub1data[sub1data$RUN == 2, ]$variable)
-    sub1data[sub1data$RUN == 2, ]$variable = sub1data[sub1data$RUN == 2, ]$variable - max(sub1data[sub1data$RUN == 1, ]$variable)
-    sub1data = dcast(sub1data, RUN ~ variable)
-    sub1data = sub1data[2:ncol(sub1data)]
-    write.table(sub1data, paste(i, ".txt", sep = ""), sep = "\t", na = "", col.names = FALSE, row.names = FALSE, quote = FALSE) 
-  }
+    sub1data = mdata[mdata$RECORDING_SESSION_LABEL == i, ]
+    colnames(sub1data)
+    if (nrow(sub1data) > 0) {
+        sub1data = sub1data[c(2:4)]
+        sub1data$variable = 1:nrow(sub1data)
+        sub1data[sub1data$RUN == 3, ]$variable = sub1data[sub1data$RUN == 3, ]$variable - max(sub1data[sub1data$RUN == 2, ]$variable)
+        sub1data[sub1data$RUN == 2, ]$variable = sub1data[sub1data$RUN == 2, ]$variable - max(sub1data[sub1data$RUN == 1, ]$variable)
+        sub1data = dcast(sub1data, RUN ~ variable)
+        sub1data = sub1data[2:ncol(sub1data)]
+        write.table(sub1data, paste(i, ".txt", sep = ""), sep = "\t", na = "", col.names = FALSE, row.names = FALSE, quote = FALSE)
+    }
 }
 
 summary(mdata$RECORDING_SESSION_LABEL)
