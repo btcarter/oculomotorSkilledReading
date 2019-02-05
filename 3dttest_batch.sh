2804d556a96c07a3cb1dffe6edfb83abba3babca
btcarter <benjamin.t.carter@gmail.com>
2019-02-05 10:07:25 -0700

    updating paths for 3dttest++

--- 3dttest_batch.sh
+++ 3dttest_batch.sh
@@ -10,9 +10,9 @@
 #ENVIRONMENT#
 #############
 
-HOME_DIR=/fslhome/ben88/compute/Reading/Compute_data
-SCRIPT_DIR=${HOME_DIR}/Scripts
-SUBJ_DIR=${HOME_DIR}/SubjData
+HOME_DIR=/fslhome/ben88/compute/Reading
+SCRIPT_DIR=${HOME_DIR}/analyses
+SUBJ_DIR=${HOME_DIR}/mriData/
 
 ##########
 #COMMANDS#
--- 3dttest_job.sh
+++ 3dttest_job.sh
@@ -27,35 +27,26 @@ AFNI_BIN=/fslhome/ben88/abin
 #VARIABLES#
 ###########
 
-HOME_DIR=/fslhome/ben88/compute/Reading/Compute_data
-    workDir=$HOME_DIR/SubjData
+HOME_DIR=/fslhome/ben88/compute/Reading
+    workDir=$HOME_DIR/mriData
 TEMPLATE=/fslhome/ben88/templates/Cthulhu
+GROUP=$HOME_DIR/results/oculomotor
 
 
 #mkdir Group_Analysis/whatever the analysis is
-if [ ! -d $HOME_DIR/Group_Analysis/oculomotor ]
+if [ ! -d $GROUP ]
     then
-        mkdir -p $HOME_DIR/Group_Analysis/oculomotor
+        mkdir -p $GROUP
 fi
-
-#To make a group mask, into the group directory
-
-#if [ ! -f $TEMPLATE/cthulhu_gm_resamp* ]
-#    then
-#        cd $TEMPLATE
-#        3dfractionize -template ${workDir}/Luke_Reading_S1/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc -prefix cthulhu_gm_resamp -clip .5 -input cthulhu_mni_GM.nii.gz
-#fi
-#now, do a t-test on beta(s)
-
-GROUP=$HOME_DIR/Group_Analysis/oculomotor
 
 cd $GROUP
 
+# perform the T-test
 $AFNI_BIN/3dttest++ \
 -Clustsim \
 -prefix fixations \
 -mask $TEMPLATE/fractionize+tlrc.BRIK.gz \
--setA FIX \
+-setA fix-cross \
 Luke_Reading_S1 ${workDir}/Luke_Reading_S1/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[1]' \
 Luke_Reading_S2 ${workDir}/Luke_Reading_S2/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[1]' \
 Luke_Reading_S3 ${workDir}/Luke_Reading_S3/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[1]' \
@@ -103,7 +94,7 @@ $AFNI_BIN/3dttest++ \
 -Clustsim \
 -prefix saccades \
 -mask $TEMPLATE/fractionize+tlrc.BRIK.gz \
--setA SAC \
+-setA sac \
 Luke_Reading_S1 ${workDir}/Luke_Reading_S1/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[3]' \
 Luke_Reading_S2 ${workDir}/Luke_Reading_S2/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[3]' \
 Luke_Reading_S3 ${workDir}/Luke_Reading_S3/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[3]' \
@@ -145,53 +136,6 @@ Luke_Reading_S52 ${workDir}/Luke_Reading_S52/afni_data/oculomotor/oculomotor_dec
 Luke_Reading_S54 ${workDir}/Luke_Reading_S54/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[3]' \
 Luke_Reading_S55 ${workDir}/Luke_Reading_S55/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[3]' \
 Luke_Reading_S56 ${workDir}/Luke_Reading_S56/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[3]'
-
-$AFNI_BIN/3dttest++ \
--Clustsim \
--prefix sac-fix \
--mask $TEMPLATE/fractionize+tlrc.BRIK.gz \
--setA Sac-fix \
-Luke_Reading_S1 ${workDir}/Luke_Reading_S1/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S2 ${workDir}/Luke_Reading_S2/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S3 ${workDir}/Luke_Reading_S3/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S4 ${workDir}/Luke_Reading_S4/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S5 ${workDir}/Luke_Reading_S5/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S6 ${workDir}/Luke_Reading_S6/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S7 ${workDir}/Luke_Reading_S7/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S8 ${workDir}/Luke_Reading_S8/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S9 ${workDir}/Luke_Reading_S9/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S11 ${workDir}/Luke_Reading_S11/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S12 ${workDir}/Luke_Reading_S12/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S13 ${workDir}/Luke_Reading_S13/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S14 ${workDir}/Luke_Reading_S14/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S16 ${workDir}/Luke_Reading_S16/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S17 ${workDir}/Luke_Reading_S17/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S18 ${workDir}/Luke_Reading_S18/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S19 ${workDir}/Luke_Reading_S19/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S20 ${workDir}/Luke_Reading_S20/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S22 ${workDir}/Luke_Reading_S22/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S23 ${workDir}/Luke_Reading_S23/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S24 ${workDir}/Luke_Reading_S24/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S25 ${workDir}/Luke_Reading_S25/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S26 ${workDir}/Luke_Reading_S26/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S27 ${workDir}/Luke_Reading_S27/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S28 ${workDir}/Luke_Reading_S28/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S29 ${workDir}/Luke_Reading_S29/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S30 ${workDir}/Luke_Reading_S30/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S31 ${workDir}/Luke_Reading_S31/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S33 ${workDir}/Luke_Reading_S33/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S35 ${workDir}/Luke_Reading_S36/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S36 ${workDir}/Luke_Reading_S37/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S37 ${workDir}/Luke_Reading_S38/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S45 ${workDir}/Luke_Reading_S45/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S47 ${workDir}/Luke_Reading_S47/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S48 ${workDir}/Luke_Reading_S48/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S50 ${workDir}/Luke_Reading_S50/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S51 ${workDir}/Luke_Reading_S51/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S52 ${workDir}/Luke_Reading_S52/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S54 ${workDir}/Luke_Reading_S54/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S55 ${workDir}/Luke_Reading_S55/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]' \
-Luke_Reading_S56 ${workDir}/Luke_Reading_S56/afni_data/oculomotor/oculomotor_deconv_blur5_ANTS_resampled+tlrc'[5]'
 
 cd ${workDir}
 
